91e0366252906f9507d68cb0f7204798de02eaab
Harshit Anand <harshitanand97@gmail.com>
2022-02-18 20:35:35 +0530

    Fix: logic

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -2,10 +2,10 @@ const listMenu = document.querySelector(".bi-list");
 const collapseMenu = document.querySelector(".nav-bar-collapse");
 
 listMenu.addEventListener("click",()=>{
-    if(collapseMenu.style.display==="none"){
-        collapseMenu.style.display="block";
+    if(collapseMenu.style.display==="block"){
+        collapseMenu.style.display="none";
     }
     else{
-        collapseMenu.style.display="none"
+        collapseMenu.style.display="block";
     }
 });
